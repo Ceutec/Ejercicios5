70eb06f79f8b6bf23611aba162df858cc4d76938
turupawn <ahmed.hn.43@gmail.com>
2018-06-09 08:15:42 -0600

    first commit

---
+++ src/ejercicios5/Exercises.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios5;
+
+/**
+ *
+ * @author turupawn
+ */
+public class Exercises {
+
+	static void writeString(String filename, String content)
+	{
+            return;
+	}
+	
+	static void writeInt(String filename, int content)
+	{
+            return;
+	}
+	
+	//Devuelve el primer entero contenido en el archivo con nombre nombre_archivo
+	static int readInt(String filename)
+	{
+            return -1;
+	}
+	
+	static String readString(String filename)
+	{
+            return "";
+	}
+	
+	static double readDouble(String filename)
+	{
+            return -1;
+	}
+	
+	static int count(String filename, String search_value)
+	{
+            return -1;
+	}
+	
+	static int getAverage(String filename)
+	{
+            return -1;
+	}
+    
+}
